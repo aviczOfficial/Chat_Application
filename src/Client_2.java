1388f1624985c232b6b7e239b33850b5efd1fa2f
aviczOfficial <ashgroover007@gmail.com>
2022-08-16 02:28:48 +0530

    C3

---
+++ src/Client_2.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Client_2 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/UserLoginForm.fxml"))));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Group Chat Application");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+}
---
+++ src/Client_3.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Client_3 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/UserLoginForm.fxml"))));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Group Chat Application");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+
+
+
+    }
+}
